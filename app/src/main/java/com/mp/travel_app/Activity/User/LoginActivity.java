ba4c7e99d34b93b8ee1f6b127682feaec34aa397
pdmv <2151013110vuong@ou.edu.vn>
2024-12-18 15:04:19 +0700

    feat: handle login function

--- app/src/main/java/com/mp/travel_app/Activity/User/LoginActivity.java
+++ app/src/main/java/com/mp/travel_app/Activity/User/LoginActivity.java
@@ -1,26 +1,113 @@
 package com.mp.travel_app.Activity.User;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
 
-import com.mp.travel_app.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.mp.travel_app.Activity.Admin.AdminDashboardActivity;
+import com.mp.travel_app.Activity.BaseActivity;
+import com.mp.travel_app.databinding.ActivityLoginBinding;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
+    ActivityLoginBinding binding;
+    DatabaseReference databaseReference;
+    public static final String[] roles = {"Admin", "Customer", "TourGuide"};
+
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        initRoleChoice();
+
+        binding.btnBack.setOnClickListener(v -> back());
+
+        binding.btnLogin.setOnClickListener(v -> {
+            username = binding.txtUsername.getText().toString();
+            password = binding.txtPassword.getText().toString();
+            String role = binding.roleSpinner.getSelectedItem().toString();
+
+            binding.btnLogin.setEnabled(false);
+            login(role);
+            binding.btnLogin.setEnabled(true);
         });
+    }
+
+    public void login(String role) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference(role);
+
+        Query query = databaseReference.orderByChild("username").equalTo(username);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        String passwordFromDatabase =
+                                dataSnapshot.child("password").getValue(String.class);
+
+                        if (password.equals(passwordFromDatabase)) {
+                            // Login success
+                            toActivity(AdminDashboardActivity.class);
+                        } else {
+                            // Login failed
+                            Log.d("LoginActivity",
+                                    String.format("Login failed. Password: %s, Password from database: %s",
+                                            password, passwordFromDatabase));
+                            showToast(LoginActivity.this, "Login failed. Please check your password.");
+                        }
+                    }
+                } else {
+                    // Login failed
+                    Log.d("LoginActivity",
+                            String.format("Login failed. Username: %s, Password: %s",
+                                    username, password));
+                    showToast(LoginActivity.this, "Login failed. Please check your username.");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("LoginActivity", error.getMessage());
+                showToast(LoginActivity.this, "An error occurred. Please try again later.");
+            }
+        });
+    }
+
+    public void back() {
+        finish();
+    }
+
+    private void initRoleChoice() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, roles);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.roleSpinner.setAdapter(adapter);
+    }
+
+    public void toActivity(Class<?> toActivity) {
+         Intent intent = new Intent(LoginActivity.this, toActivity);
+         startActivity(intent);
+         finish();
+    }
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 }
