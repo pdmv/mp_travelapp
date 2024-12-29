4bba05f96d1677f8e0ee059897f6f230e50ffd91
pdmv <2151013110vuong@ou.edu.vn>
2024-12-29 22:18:28 +0700

    chore: add activity into manifest

--- app/src/main/java/com/mp/travel_app/Activity/User/TicketManagementActivity.java
+++ app/src/main/java/com/mp/travel_app/Activity/User/TicketManagementActivity.java
@@ -1,9 +1,11 @@
 package com.mp.travel_app.Activity.User;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,6 +17,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.mp.travel_app.Adapter.TicketAdapter;
 import com.mp.travel_app.Domain.Ticket;
+import com.mp.travel_app.Domain.Users;
+import com.mp.travel_app.Utils.Common;
 import com.mp.travel_app.databinding.ActivityTicketManagementBinding;
 
 import java.util.ArrayList;
@@ -31,30 +35,37 @@ public class TicketManagementActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Inflating layout bằng ViewBinding
         binding = ActivityTicketManagementBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Khởi tạo Database Reference
-        database = FirebaseDatabase.getInstance().getReference("Tickets");
+        database = FirebaseDatabase.getInstance().getReference("Ticket");
 
-        // Khởi tạo RecyclerView và Adapter
         ticketsRecyclerView = binding.recyclerViewTickets;
         ticketList = new ArrayList<>();
         ticketAdapter = new TicketAdapter(ticketList);
         ticketsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         ticketsRecyclerView.setAdapter(ticketAdapter);
 
-        String userId = "example_user_id";
-        fetchUserTickets(userId);
+        Common.getCurrentUser(this, new Common.GetUserCallback() {
+            @Override
+            public void onSuccess(Users user) {
+                fetchUserTickets(user.getId());
+            }
+
+            @Override
+            public void onFailure(String errorMessage) {
+
+            }
+        });
     }
 
 
     private void fetchUserTickets(String userId) {
 
         database.orderByChild("customer/id").equalTo(userId).addValueEventListener(new ValueEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 ticketList.clear();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     Ticket ticket = snapshot.getValue(Ticket.class);
@@ -66,7 +77,7 @@ public class TicketManagementActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
                 Toast.makeText(TicketManagementActivity.this, "Failed to load tickets: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
